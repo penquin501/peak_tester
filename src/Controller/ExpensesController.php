659e4fb01c504a3c2aab3bb4573176a03665f15e
Nattharee Pisarnsathukit <penquin501@gmail.com>
2019-09-07 09:25:32 +0700

    hide query billing

--- src/Controller/ExpensesController.php
+++ src/Controller/ExpensesController.php
@@ -16,7 +16,7 @@ class ExpensesController extends AbstractController
     {
         $entityManager = $this->getDoctrine()->getManager('default');
         $customEntityManager = $this->getDoctrine()->getManager('custom');
-        $output=[];
+
         $sqlPeakRequest="SELECT bill_no, json_send, json_result ".
                         "FROM peak_prepare_to_send ".
                         "WHERE peak_method='expenses' AND DATE(recorddate)>='2019-09-04'";
@@ -27,22 +27,24 @@ class ExpensesController extends AbstractController
         foreach ($dataPeak as $item) {
             $dataJsonSend=json_decode($item['json_send'],true);
             dd($dataJsonSend['PeakExpenses']['expenses']);
-            $sqlMerchantBilling = "SELECT mb.parcel_bill_no,SUM(mb.payment_amt) as sumPayAmt,SUM(mb.payment_discount) as sumDiscount,Sum(mb.transportprice) as sumTransPrice,SUM(md.productcost) as sumCost " .
-                "FROM merchant_billing mb " .
-                "JOIN merchant_billing_detail md " .
-                "ON mb.takeorderby=md.takeorderby and mb.payment_invoice=md.payment_invoice " .
-                "WHERE mb.parcel_bill_no = '" . $item['bill_no']."'";
-            $merchantBillingInfo=$entityManager->getConnection()->query($sqlMerchantBilling);
-            $dataMerchantBilling=json_decode($this->json($merchantBillingInfo)->getContent(),true);
-            dd($dataMerchantBilling);
+            foreach($dataJsonSend['PeakExpenses']['expenses'] as $itemExpenses) {
+//                dd($itemExpenses['tags'][1]);
+                $strBillNo=$itemExpenses['tags'][1];
+                $exBillNo=explode("|",$strBillNo);
+//                dd($exBillNo[1]);
+            }
+
+//            $sqlMerchantBilling = "SELECT mb.parcel_bill_no,SUM(mb.payment_amt) as sumPayAmt,SUM(mb.payment_discount) as sumDiscount,Sum(mb.transportprice) as sumTransPrice,SUM(md.productcost) as sumCost " .
+//                "FROM merchant_billing mb " .
+//                "JOIN merchant_billing_detail md " .
+//                "ON mb.takeorderby=md.takeorderby and mb.payment_invoice=md.payment_invoice " .
+//                "WHERE mb.parcel_bill_no = '" . $item['bill_no']."'";
+//            $merchantBillingInfo=$entityManager->getConnection()->query($sqlMerchantBilling);
+//            $dataMerchantBilling=json_decode($this->json($merchantBillingInfo)->getContent(),true);
+
         }
-
-//        $sql="SELECT bill_no,json_send,json_result FROM peak_prepare_to_send WHERE peak_method='expenses' AND Date(recorddate) >= '2019-09-01'";
-//        $id = $request->query->get('id');
-
-//        $query="SELECT T01_AFA_BENEFIT_FNC_GET_TODAY_SALE_AMT(A.id) AS T01_TOTAL_SALE_AMT_TODAY FROM VW01_AFA_BENEFIT A WHERE A.id =".$id;
-
-
+//        dd($exBillNo[1]);//ใช้ได้
+        $output=['billNo'=>$exBillNo[1]];
         return $this->json($output);
     }
 }
